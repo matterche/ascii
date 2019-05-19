107f25afcc424af8de8c371f556be979c7c0015d
Matthias Erche <matthias.erche@zalando.de>
2019-05-19 21:32:13 +0200

    Add some basic logging

--- app/ascii/AsciiController.scala
+++ app/ascii/AsciiController.scala
@@ -2,6 +2,7 @@ package ascii
 
 import common.ServiceError
 import javax.inject._
+import play.api.Logger
 import play.api.mvc._
 
 import scala.concurrent.Future
@@ -9,13 +10,17 @@ import scala.concurrent.Future
 @Singleton
 class AsciiController @Inject()(cc: ControllerComponents, asciiService: AsciiService) extends AbstractController(cc) {
 
+  private val log = Logger(getClass)
+
   def registerImage(): Action[ImageDTO] = Action.async(parse.json[ImageDTO]) { request =>
     val imageDto = request.body
 
     Future.successful {
-      asciiService.createImage(imageDto) match {
-        case Right(_)                  => Created("")
-        case Left(error: ServiceError) => error.toResult
+      asciiService.registerImage(imageDto) match {
+        case Right(_) => Created("")
+        case Left(error: ServiceError) =>
+          log.warn(s"Service error while registering an image occurred: " + error.message)
+          error.toResult
       }
     }
   }
@@ -25,8 +30,10 @@ class AsciiController @Inject()(cc: ControllerComponents, asciiService: AsciiSer
 
     Future.successful {
       asciiService.uploadChunk(sha256, chunkDto) match {
-        case Right(_)                  => Created("")
-        case Left(error: ServiceError) => error.toResult
+        case Right(_) => Created("")
+        case Left(error: ServiceError) =>
+          log.warn(s"Service error while upload an image chunk occurred: " + error.message)
+          error.toResult
       }
     }
   }
@@ -34,8 +41,10 @@ class AsciiController @Inject()(cc: ControllerComponents, asciiService: AsciiSer
   def downloadImage(sha256: String): Action[AnyContent] = Action.async { _ =>
     Future.successful {
       asciiService.downloadImage(sha256) match {
-        case Right(rawImage)           => Ok(rawImage)
-        case Left(error: ServiceError) => error.toResult
+        case Right(rawImage) => Ok(rawImage)
+        case Left(error: ServiceError) =>
+          log.warn(s"Service error while downloading an image occurred: " + error.message)
+          error.toResult
       }
     }
   }
--- app/common/ErrorHandler.scala
+++ app/common/ErrorHandler.scala
@@ -1,6 +1,7 @@
 package common
 
 import javax.inject.Singleton
+import play.api.Logger
 import play.api.http.HttpErrorHandler
 import play.api.libs.json.Json
 import play.api.mvc.Results.{InternalServerError, Status}
@@ -11,14 +12,18 @@ import scala.concurrent.Future
 @Singleton
 class ErrorHandler extends HttpErrorHandler {
 
+  private val log = Logger(getClass)
+
   override def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
     Future.successful {
+      log.warn(s"Service error occurred: " + message)
       Status(statusCode)(Json.toJson(ProblemDTO(s"$statusCode", message)))
     }
   }
 
   override def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
     Future.successful {
+      log.error(s"Unexpected error occurred: ", exception)
       InternalServerError(Json.toJson(ProblemDTO("500", "Unexpected error occurred" + exception.getLocalizedMessage)))
     }
   }
